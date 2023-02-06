48b0d7caa5b73539b81a770954cc3add68b4148b
Chris Bloomfield <43499897+ithinkandicode@users.noreply.github.com>
2023-02-06 12:11:51 +0000

    add gulp + upgrade to scss + move assets

---
+++ www/gulpfile.js
@@ -0,0 +1,136 @@
+
+// Dependancies
+// ============================================================================
+
+const autoprefixer = require("autoprefixer");
+const browsersync  = require("browser-sync").create();
+const cssnano      = require("cssnano");
+const del          = require("del");
+const gulp         = require("gulp");
+const imagemin     = require("gulp-imagemin");
+const newer        = require("gulp-newer");
+const plumber      = require("gulp-plumber");
+const postcss      = require("gulp-postcss");
+const rename       = require("gulp-rename");
+const sass         = require("gulp-sass");
+const sourcemaps   = require("gulp-sourcemaps");
+
+
+// Config
+// ============================================================================
+
+const paths = {
+    styles: {
+        src:  './assets/styles/src/**/*.scss',
+        dest: './assets/styles/dist/'
+    },
+    watchFiles: [
+        './*.html',
+    ],
+};
+
+
+// Styles
+// ============================================================================
+
+// Clean Styles
+function clean()
+{
+    return del( ["./assets/styles/dist"] );
+}
+
+// Optimize Styles
+function styles()
+{
+    const plugins = [
+        autoprefixer({ browsers: ["last 2 versions"] }),
+        cssnano()
+    ];
+
+    return gulp
+        .src(paths.styles.src)
+        .pipe(
+            plumber({
+                errorHandler: function(err) {
+                    console.log(err);
+                    this.emit("end");
+                }
+            })
+        )
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: "expanded" }))
+        .pipe(postcss(plugins))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.styles.dest))
+        // .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest(paths.styles.dest))
+
+        // Stream changes to bsync
+        // https://github.com/BrowserSync/browser-sync/issues/955#issuecomment-218927650
+        .pipe(browsersync.stream( { match: '**/*.css' } ));
+}
+
+
+// Browser Sync
+// ============================================================================
+
+const bsyncOptions = {
+	// Proxy an EXISTING vhost
+	// proxy: 'https://brotato-items.localdev',
+
+	// Don't show any notifications in the browser.
+	notify: false, // default: true
+
+	// Clicks, Scrolls & Form inputs on any device will be mirrored to all others
+	ghostMode: {
+		clicks: false, // default: true
+		forms:  false, // default: true
+		scroll: false, // default: false
+	},
+
+    // Log information about changed files (default: true)
+    // logFileChanges: true,
+    logFileChanges: false,
+
+    watch: true,
+
+    injectChanges: true,
+    // logLevel: "debug"
+}
+
+
+// Watch
+// ============================================================================
+
+// Watch Files
+function watch()
+{
+    browsersync.init( bsyncOptions );
+
+    gulp.watch( paths.styles.src, styles );
+
+    // Hotfix for css changes not reloading
+    // gulp.watch( paths.styles.src ).on( 'change', browsersync.reload() );
+
+    gulp.watch( paths.watchFiles ).on( 'change', browsersync.reload );
+}
+
+
+// Tasks
+// ============================================================================
+
+// Build Assets and start watching files
+const build = gulp.series(
+    clean,
+    gulp.parallel( styles ),
+    watch
+);
+
+// Tasks
+exports.clean = clean;
+exports.styles = styles;
+exports.watch = watch;
+exports.build = build;
+
+// Default Task
+exports.default = build;
